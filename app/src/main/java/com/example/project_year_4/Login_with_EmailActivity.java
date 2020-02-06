a6287984eb71e31fea3d982b3eeedd10d4accbb8
NeramitIT <45871275+NeramitIT@users.noreply.github.com>
2020-02-06 13:03:00 +0700

    อัพกันพัง

--- app/src/main/java/com/example/project_year_4/Login_with_EmailActivity.java
+++ app/src/main/java/com/example/project_year_4/Login_with_EmailActivity.java
@@ -4,20 +4,70 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login_with_EmailActivity extends AppCompatActivity {
+
+
+    private EditText mPswEditText;
+    private TextView mToggleTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_with__email);
 
-        Button Login_btn = (Button)findViewById(R.id.login_btn);
-        Button Forgot_btn = (Button)findViewById(R.id.Forgot_btn);
-        Button Regis_btn = (Button)findViewById(R.id.regis_btn);
+        Button Login_btn = (Button) findViewById(R.id.login_btn);
+        Button Forgot_btn = (Button) findViewById(R.id.Forgot_btn);
+        Button Regis_btn = (Button) findViewById(R.id.regis_btn);
 
+        mPswEditText = (EditText) findViewById(R.id.pass_txt);
+        mToggleTextView = (TextView) findViewById(R.id.toggleTxt);
+        mPswEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+        mPswEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(mPswEditText.getText().length()>0){
+                    mToggleTextView.setVisibility(View.VISIBLE);
+                }
+                else {
+                    mToggleTextView.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+        mToggleTextView.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                if(mToggleTextView.getText()=="SHOW") {
+                    mToggleTextView.setText("HIDE");
+                    mPswEditText.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    mPswEditText.setSelection(mPswEditText.length());
+                }
+                else{
+                    mToggleTextView.setText("SHOW");
+                    mPswEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    mPswEditText.setSelection(mPswEditText.length());
+                }
+            }
+        });
 
 
 
--- app/src/main/java/com/example/project_year_4/New_PasswordActivity.java
+++ app/src/main/java/com/example/project_year_4/New_PasswordActivity.java
@@ -1,16 +1,13 @@
 package com.example.project_year_4;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import com.google.android.material.bottomappbar.BottomAppBar;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class New_PasswordActivity extends AppCompatActivity {
 
@@ -20,7 +17,33 @@ public class New_PasswordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new__password);
 
+        BottomNavigationView bottomNav= findViewById(R.id.bottom_navigation_view);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment =null;
+
+            switch (menuItem.getItemId()){
+                case R.id.nav_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.nav_progress:
+                    selectedFragment = new ProgressFragment();
+                    break;
+                case R.id.nav_profile:
+                    selectedFragment = new ProfileFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+
+            return true;
+        }
+    };
 }
 
 
